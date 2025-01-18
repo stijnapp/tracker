c41d19d9d34bdb06076b55f80a16f3008d3242cc
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-18 16:34:34 +0100

    updated notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 
+// https://flowbite.com/blocks/marketing/404/#default-example
 export default function NotFound() {
     return (
-        <section className="flex flex-col gap-4">
-            <h1 className="text-2xl font-bold text-primary">404 Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-            <Link to="/" className="btn-primary">Back to home</Link>
+        <section className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div className="mx-auto max-w-screen-sm text-center">
+                <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600">404</h1>
+                <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">Something&apos;s missing.</p>
+                <p className="mb-4 text-lg font-light text-gray-500">Sorry, we can&apos;t find that page. You&apos;ll find lots to explore on the home page. </p>
+                <Link to="/" className="btn-primary">Back to home</Link>
+            </div>
         </section>
     )
 }
