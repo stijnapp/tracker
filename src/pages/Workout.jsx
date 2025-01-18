5d954141f3fe8479b55c7f2db0a8f58c4406c4fc
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-19 00:51:07 +0100

    remove unused workflow

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -3,7 +3,7 @@ import Page from "../components/Page";
 export default function Workout() {
     return (
         <Page title="Workout">
-            ... test
+            ...
         </Page>
     )
 }
