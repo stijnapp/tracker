99e80081c9a4ad69afc361f04d40e12b01661b5d
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-19 16:56:58 +0100

    darkmode toggle

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,13 @@
 import Page from "../components/Page";
 
 export default function Settings() {
+    const toggleDarkmode = () => {
+        document.documentElement.classList.toggle('dark')
+    }
+
     return (
         <Page title="Settings">
-            ...
+            <button onClick={toggleDarkmode} className="btn-primary">Toggle darkmode</button>
         </Page>
     )
 }
