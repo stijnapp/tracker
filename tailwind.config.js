7304a627a2761bc0782a05d914595479c425282a
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-19 16:56:47 +0100

    fixed colors somewhat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'selector',
   theme: {
     extend: {
       colors: {
-        primary: {
-          DEFAULT: '#48A9A6',
-          light: '#65bdba',
-          dark: '#388482',
-        },
-        dark: '#2F323A',
+        primary: '#1d8098',
+        secondary: '#ff6347',
+        danger: '#ef4444',
+        warning: '#eab308',
+        success: '#16a34a',
+        info: '#3b82f6',
+        light: '#f3f4f6',
+        dark: '#2f323a',
       },
     },
   },
