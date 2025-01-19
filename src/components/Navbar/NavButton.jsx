721b6738d2fe7d35b45ebed37dc57d4518ec96c8
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-19 12:54:46 +0100

    responsive nav buttons

--- src/components/Navbar/NavButton.jsx
+++ src/components/Navbar/NavButton.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
  * @param {object{ to: string, icon: IconProp, text: string }} props 
  */
 export default function NavButton({ to, icon, text }) {
-    const navButtonClasses = 'flex flex-col gap-1 text-center justify-center text-sm pb-1 pt-3 transition-all duration-200';
+    const navButtonClasses = 'flex flex-col basis-full gap-1 text-center justify-center text-sm pb-1 pt-3 transition-all duration-200';
 
     return (
         <NavLink to={to} className={({ isActive }) => `${navButtonClasses} ${isActive ? 'text-primary -translate-y-1 font-bold drop-shadow-lg' : 'text-gray-500 font-medium'}`}>
