95e1cf6bab2bc8701ca9958cb473ebd282413c80
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-20 22:07:55 +0100

    moved hook and added jsdoc

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Custom hook to manage state with localStorage.
+ *
+ * @param {string} key - The key under which the value is stored in localStorage.
+ * @param {any} initialValue - The initial value to use if there is no value in localStorage.
+ * @returns {[any, (value: any) => void]} - Returns the current value and a function to update it.
+ */
 export default function useLocalStorage(key, initialValue) {
     const [value, setValue] = useState(() => {
         const jsonValue = localStorage.getItem(key);
