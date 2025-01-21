5e504da4ab4474e13e50673db974e7524967ebb5
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-21 21:29:17 +0100

    unchecked radiobutton dark background

--- src/components/Form/RadioButtonGroup.jsx
+++ src/components/Form/RadioButtonGroup.jsx
@@ -19,7 +19,7 @@ export default function RadioButtonGroup({ label, options, value, setValue, clas
                     <label key={optionValue} htmlFor={optionValue} className="flex justify-between gap-4 items-center p-2 rounded-lg ring-1 ring-transparent has-[:checked]:ring-primary has-[:checked]:text-primary has-[:checked]:bg-primary/10">
                         {icon && <FontAwesomeIcon icon={icon} className="aspect-square" />}
                         <p className="flex-auto">{optionLabel}</p>
-                        <input type="radio" name={optionValue} id={optionValue} checked={value === optionValue} onChange={() => setValue(optionValue)} className="box-content h-2 w-2 appearance-none rounded-full border-[5px] border-floating-light bg-floating-light bg-clip-padding outline-none ring-1 ring-gray-950/10 checked:border-primary checked:ring-primary" />
+                        <input type="radio" name={optionValue} id={optionValue} checked={value === optionValue} onChange={() => setValue(optionValue)} className="box-content h-2 w-2 appearance-none rounded-full border-[5px] border-floating-light dark:border-floating-dark bg-floating-light dark:bg-floating-dark bg-clip-padding outline-none ring-1 ring-black/20 dark:ring-white/10 checked:border-primary checked:ring-primary" />
                     </label>
                 ))}
             </ul>
