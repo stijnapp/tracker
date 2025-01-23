d58a2aa6d06518bda45ba8abc5fc23d721f88ff7
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-23 21:13:54 +0100

    long page for testing + disabled btns

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -8,7 +8,7 @@ export default function Stats() {
 
     return (
         <Page title="Stats">
-            <Card title="Button test" className="flex flex-col gap-1">
+            <Card title="Button test">
                 <p className="font-semibold">Basic</p>
                 <div className="flex flex-row flex-wrap gap-2 transition-transform duration-200">
                     <button className="btn-primary">Primary</button>
@@ -23,9 +23,25 @@ export default function Stats() {
                 </div>
             </Card>
 
-            <Card title="Alert test">
-                <button className="btn-danger mb-4" onClick={() => setError('Lorem ipsum dolor sit amet conse tetur, adipisicing elit. Laboriosam, eaque.')}>Show alert</button>
+            <Card title="Disabled buttons">
+                <p className="font-semibold">Basic</p>
+                <div className="flex flex-row flex-wrap gap-2 transition-transform duration-200">
+                    <button className="btn-primary" disabled>Primary</button>
+                    <button className="btn-secondary" disabled>Secondary</button>
+                </div>
+                <p className="font-semibold mt-4">Extra</p>
+                <div className="flex flex-row flex-wrap gap-2 transition-transform duration-200">
+                    <button className="btn-danger" disabled>Danger</button>
+                    <button className="btn-warning" disabled>Warning</button>
+                    <button className="btn-success" disabled>Success</button>
+                    <button className="btn-info" disabled>Info</button>
+                </div>
+            </Card>
+
+            <Card title="Alert test" className="flex flex-col gap-4">
+                <button className="btn-danger" onClick={() => setError('Lorem ipsum dolor sit amet conse tetur, adipisicing elit. Laboriosam, eaque.')}>Show alert</button>
                 <Alert message={error} setMessage={setError} autoClose />
+                <button className="btn-secondary" disabled>asdf</button>
             </Card>
         </Page>
     )
