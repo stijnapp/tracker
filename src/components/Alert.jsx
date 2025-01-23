0328a60ca1ca28371d5ec877369618dbb3b10853
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-23 16:27:20 +0100

    getMsFromDuration function + some auto close stuff

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -9,15 +9,16 @@ import { useEffect, useState } from "react";
  *  setMessage: (value: string) => void,
  *  type?: 'danger' | 'warning' | 'success' | 'info',
  *  isCloseable?: boolean,
- *  autoCloseAfterMs?: number | null,
+ *  autoClose?: boolean,
  *  className?: string
  * }} props
  */
-export default function Alert({ message, setMessage, type = "danger", isCloseable = true, autoCloseAfterMs = null, className = "" }) {
+export default function Alert({ message, setMessage, type = "danger", isCloseable = true, autoClose = true, className = "" }) {
     // TODO: implement type
-    // TODO: implement autoCloseAfterMs
     const [isHiding, setIsHiding] = useState(true)
-    const duration = "duration-200"
+    const duration = 'duration-200'
+    // const autoCloseAfterMs = 'duration-[1s]'
+    // const progressBarRef = useRef()
 
     if (setMessage === undefined) {
         console.error('Alert component requires setMessage prop')
@@ -25,6 +26,17 @@ export default function Alert({ message, setMessage, type = "danger", isCloseabl
 
     useEffect(() => {
         setIsHiding(!message)
+
+        // if (message && autoClose) {
+        //     // add w-full class
+        //     progressBarRef.current.classList.add('w-full')
+
+        //     const timeout = setTimeout(() => {
+        //         setIsHiding(true)
+        //         clearTimeout(timeout)
+        //     }, getMsFromDuration(autoCloseAfterMs))
+        // }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [message])
 
     useEffect(() => {
@@ -35,7 +47,7 @@ export default function Alert({ message, setMessage, type = "danger", isCloseabl
                 if (ignore) return
                 setMessage('')
                 clearTimeout(timeout)
-            }, duration.split('-')[1])
+            }, getMsFromDuration(duration))
         }
 
         return () => {
@@ -44,6 +56,24 @@ export default function Alert({ message, setMessage, type = "danger", isCloseabl
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isHiding])
+
+    const getMsFromDuration = (duration) => {
+        if (!duration.includes('[') || !duration.includes(']')) {
+            return parseInt(duration.replace('duration-', ''))
+        }
+
+        const value = duration.split('[')[1].split(']')[0]
+        if (value.includes('ms')) {
+            return parseInt(value.replace('ms', ''))
+        } else if (value.includes('s')) {
+            return parseInt(value.replace('s', '') * 1000)
+        } else if (value.includes('m')) {
+            return parseInt(value.replace('m', '') * 1000 * 60)
+        } else {
+            console.error('Invalid duration value', value)
+            return 0
+        }
+    }
 
     if (isHiding && !message) return null
 
@@ -54,6 +84,9 @@ export default function Alert({ message, setMessage, type = "danger", isCloseabl
             <div className="font-medium break-words brightness-[0.75]">
                 {message}
             </div>
+            {/* {autoClose && (
+                <div ref={progressBarRef} className="absolute bottom-0 left-0 h-1 bg-danger w-0 transition-all duration-[3000ms]" />
+            )} */}
             {isCloseable &&
                 <button onClick={() => setIsHiding(true)} className="flex items-center justify-center h-10 w-10 aspect-square -mr-2 rounded-md focus:ring-2 focus:ring-danger/80 hover:bg-danger/15 brightness-[0.75]" aria-label="Close">
                     <span className="sr-only">Close</span>
