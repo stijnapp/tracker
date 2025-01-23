772b207c9e8cf1791904809f3a7efd8a96099065
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-23 13:18:55 +0100

    primary color drop shadow

--- src/components/Navbar/NavButton.jsx
+++ src/components/Navbar/NavButton.jsx
@@ -9,7 +9,7 @@ export default function NavButton({ to, icon, text }) {
     const navButtonClasses = 'flex flex-col gap-1 text-center justify-center text-sm pb-1 pt-3 transition-all duration-200';
 
     return (
-        <NavLink to={to} className={({ isActive }) => `${navButtonClasses} ${isActive ? 'text-primary -translate-y-1 font-bold drop-shadow-lg' : 'text-gray-500 dark:text-gray-400 font-medium'}`}>
+        <NavLink to={to} className={({ isActive }) => `${navButtonClasses} ${isActive ? 'text-primary -translate-y-1 font-bold drop-shadow-[0_0px_8px_#1d809866]' : 'text-gray-500 dark:text-gray-400 font-medium'}`}>
             <FontAwesomeIcon icon={icon} size="xl" />
             {text}
         </NavLink>
