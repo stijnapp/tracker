9017edab1773ccfae5c338a2aeb908102370aead
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-23 14:34:52 +0100

    prep work for import/export

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -15,10 +15,15 @@ export default function Settings() {
     ]
 
     return (
-        <Page title="Settings">
+        <Page title="Settings" className="flex flex-col gap-4">
             <Card>
                 <RadioButtonGroup label="Theme" options={themeOptions} value={theme} setValue={setTheme} />
             </Card>
+            {/* <Card title="Data" className="flex gap-4">
+                TODO: make export primary if last export was more than 7 days ago
+                <button className="btn-secondary w-full">Export Data<FontAwesomeIcon icon={faUpload} className="ml-2" /></button>
+                <button className="btn-secondary w-full">Import Data<FontAwesomeIcon icon={faDownload} className="ml-2" /></button>
+            </Card> */}
         </Page>
     )
 }
