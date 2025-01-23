edae7bc301a0fcb31fe126441b0024743dff007b
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-23 15:23:12 +0100

    updated setting page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -7,6 +7,8 @@ import useTheme from "../hooks/useTheme";
 
 export default function Settings() {
     const [theme, setTheme] = useTheme()
+    // // TODO: implement export and import data
+    // const [promoteExport, setPromoteExport] = useState(true)
 
     const themeOptions = [
         { value: 'light', label: 'Light', icon: faSun },
@@ -16,12 +18,11 @@ export default function Settings() {
 
     return (
         <Page title="Settings" className="flex flex-col gap-4">
-            <Card>
-                <RadioButtonGroup label="Theme" options={themeOptions} value={theme} setValue={setTheme} />
+            <Card title="Appearance">
+                <RadioButtonGroup label="Theme" options={themeOptions} value={theme} setValue={setTheme} hideLabel />
             </Card>
             {/* <Card title="Data" className="flex gap-4">
-                TODO: make export primary if last export was more than 7 days ago
-                <button className="btn-secondary w-full">Export Data<FontAwesomeIcon icon={faUpload} className="ml-2" /></button>
+                <button className={`${promoteExport ? 'btn-primary' : 'btn-secondary'} w-full`} onClick={() => setPromoteExport(false)}>Export Data<FontAwesomeIcon icon={faUpload} className="ml-2" /></button>
                 <button className="btn-secondary w-full">Import Data<FontAwesomeIcon icon={faDownload} className="ml-2" /></button>
             </Card> */}
         </Page>
