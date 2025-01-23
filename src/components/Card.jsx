4fe8d7cefa1645eeaae0aa208a516962669772b9
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-23 15:22:05 +0100

    improved a11y + added optional line

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,24 @@
 /**
  * @param {{
 *  title?: string,
+*  showLine?: boolean,
 *  className?: string
 *  children: JSX.Element | JSX.Element[]
 * }} args
 * @returns {JSX.Element}
 */
-export default function Card({ title = "", className = "", children }) {
+export default function Card({ title = "", showLine = false, className = "", children }) {
     return (
-        <div className="p-4 rounded-lg shadow bg-floating-light dark:bg-floating-dark theme-transition">
-            {title && <h1 className="font-semibold mb-2">{title}</h1>}
+        <article className="p-4 rounded-lg shadow bg-floating-light dark:bg-floating-dark theme-transition">
+            {title &&
+                <>
+                    <h1 className="mb-3 font-semibold text-lg">{title}</h1>
+                    {showLine && <hr className="h-px mb-4 bg-gray-400/30 border-0" />}
+                </>
+            }
             <div className={className}>
                 {children}
             </div>
-        </div>
+        </article>
     )
 }
