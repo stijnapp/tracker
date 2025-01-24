57b8c8f3989bfe21ab48910d7c2f2182dcf54da1
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-24 15:22:51 +0100

    commented card line option

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,13 +7,13 @@
 * }} args
 * @returns {JSX.Element}
 */
-export default function Card({ title = "", showLine = false, className = "", children }) {
+export default function Card({ title = "", /* showLine = false, */ className = "", children }) {
     return (
         <article className="p-4 rounded-lg shadow dark:shadow-md bg-floating-light dark:bg-floating-dark theme-transition">
             {title &&
                 <>
                     <h1 className="mb-3 font-semibold text-xl">{title}</h1>
-                    {showLine && <hr className="h-px mb-4 bg-gray-400/30 border-0" />}
+                    {/* {showLine && <hr className="h-px mb-4 bg-gray-400/30 border-0" />} */}
                 </>
             }
             <div className={className}>
