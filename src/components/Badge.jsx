cc66a50ca8aedfdfffecdadb714537bd66d3112b
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-24 18:30:46 +0100

    Updated jsdoc for components

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,4 +1,11 @@
-export default function Badge({ children, className = "" }) {
+/**
+ * @param {{
+*  className?: string
+*  children: JSX.Element | JSX.Element[]
+* }} args
+* @returns {JSX.Element}
+*/
+export default function Badge({ className = "", children }) {
     return (
         <span className={`px-2.5 py-0.5 rounded-md bg-primary/15 text-primary text-xs font-medium ${className}`}>
             <span className="brightness-75 dark:brightness-125">
