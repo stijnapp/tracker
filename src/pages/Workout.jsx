2cfa3bbf08c834a999ebce3d9731932a5cd2f27a
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-24 18:39:19 +0100

    some filler stuff on workout page

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import Card from "../components/Card";
 import Page from "../components/Page";
+import { getCurrentDateTime } from "../helpers/dateTime";
 
 export default function Workout() {
+    const [currentDateTime, setCurrentDateTime] = useState(getCurrentDateTime())
+
     return (
         <Page title="Workout">
-            ...
+            <Card>
+                <input type="datetime-local" value={currentDateTime} onChange={(event) => setCurrentDateTime(event.target.value)} name="" id="" />
+                <button className="btn-secondary">Finish workout</button>
+            </Card>
+
+            <Card title="Machine name" collapsible>
+                <p>Machine description</p>
+            </Card>
+
+            <button className="btn-primary">Add exercise</button>
         </Page>
     )
 }
