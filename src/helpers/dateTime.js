55a0f85cf6d5625c10da4e9e8db23a497dc48ea7
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-26 00:46:32 +0100

    added dateTime functions

--- src/helpers/dateTime.js
+++ src/helpers/dateTime.js
@@ -1,3 +1,6 @@
+const timeFormat = { hour: '2-digit', minute: '2-digit', second: '2-digit' }
+const dateFormat = { year: 'numeric', month: 'numeric', day: 'numeric' }
+
 /**
  * @param {boolean} [includeSeconds=false]
  * @returns {string}
@@ -12,4 +15,35 @@ export function getCurrentDateTime(includeSeconds = false) {
         return currentDateTime.toJSON().substring(0, 19)
     }
     return currentDateTime.toJSON().substring(0, 16)
+}
+
+// TODO: jsdoc
+export function dateToText(date = null) {
+    return new Date(date ?? getCurrentDateTime()).toLocaleDateString()
+}
+
+export function timeToText(time = null, includeSeconds = false) {
+    return new Date(time ?? getCurrentDateTime(includeSeconds)).toLocaleTimeString([], includeSeconds ? timeFormat : { hour: timeFormat.hour, minute: timeFormat.minute })
+}
+
+export function dateTimeToText(dateTime = null, includeSeconds = false) {
+    return new Date(dateTime ?? getCurrentDateTime(includeSeconds)).toLocaleString([], includeSeconds ? { ...dateFormat, ...timeFormat } : { ...dateFormat, hour: timeFormat.hour, minute: timeFormat.minute })
+}
+
+export function timeAgo(date) {
+    const seconds = Math.floor((new Date() - new Date(date)) / 1000)
+    switch (true) {
+        case seconds < 60: // 60 seconds
+            return 'just now'
+        case seconds < 60 * 60: // 60 minutes
+            return Math.floor(seconds / 60) + ' minutes ago'
+        case seconds < 60 * 60 * 24: // 24 hours
+            return Math.floor(seconds / 3600) + ' hours ago'
+        case seconds < 60 * 60 * 24 * 31: // 31 days
+            return Math.floor(seconds / 86400) + ' days ago'
+        case seconds < 60 * 60 * 24 * 365: // 365 days
+            return Math.floor(seconds / 2592000) + ' months ago'
+        default:
+            return Math.floor(seconds / 31536000) + ' years ago'
+    }
 }
