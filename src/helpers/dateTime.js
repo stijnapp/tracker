dda5664c36b4f3cc7225448fae1e3dd9bde2170c
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-25 18:09:14 +0100

    option to include seconds + jsdoc

--- src/helpers/dateTime.js
+++ src/helpers/dateTime.js
@@ -1,5 +1,15 @@
-export function getCurrentDateTime() {
+/**
+ * @param {boolean} [includeSeconds=false]
+ * @returns {string}
+ * @example
+ * getCurrentDateTime() // '2022-02-22T14:00'
+ * getCurrentDateTime(true) // '2022-02-22T14:00:00'
+ */
+export function getCurrentDateTime(includeSeconds = false) {
     const currentDateTime = new Date()
     currentDateTime.setHours(currentDateTime.getHours() + 1)
+    if (includeSeconds) {
+        return currentDateTime.toJSON().substring(0, 19)
+    }
     return currentDateTime.toJSON().substring(0, 16)
 }
