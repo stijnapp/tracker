cb92fc2462f35b498494bffc74214b61f773780a
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-30 15:47:25 +0100

    added styling for FileInput

--- src/components/Form/FileInput.jsx
+++ src/components/Form/FileInput.jsx
@@ -1,29 +1,41 @@
-export default function FileInput({ label, value, setValue, className = "", ...props }) {
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+export default function FileInput({ label, value, setValue, showPreview = false, className = "", ...props }) {
     const handleFileChange = (e) => {
         if (!e.target.files) return
         setValue(e.target.files[0])
     }
 
-    // TODO: add styling
     // TODO: add remove button
+    // TODO: improve preview
     return (
         <>
-            {label}
-            <input
-                type="file"
-                onChange={handleFileChange}
-                {...props}
-                className={`${className}`}
-            />
-            {value && <>
-                Preview:
-                <img src={URL.createObjectURL(value)} alt={value.name} className="w-full rounded-md shadow-md" />
-                <ul>
-                    <li>Name: {value.name}</li>
-                    <li>Type: {value.type}</li>
-                    <li>Size: {value.size} bytes</li>
-                </ul>
-            </>}
+            <div className={`relative ${className}`}>
+                <label>
+                    <input
+                        type="file"
+                        onChange={handleFileChange}
+                        {...props}
+                        className="floating-label-input !p-0 peer file:px-2 file:py-1.5 file:mr-2 file:rounded-md file:transition-[border,box-shadow] file:duration-200 file:bg-transparent file:dark:text-light file:border-0 file:border-r file:border-white file:dark:border-gray-300 file:focus:border-primary file:focus:ring-2 file:focus:ring-primary"
+                    />
+                    <span className="floating-label">
+                        {label}
+                    </span>
+                </label>
+            </div>
+            {value && showPreview && value.type.startsWith('image') && (
+                <div>
+                    <button className="btn-secondary" onClick={() => setValue(null)}><FontAwesomeIcon icon={faTrash} /></button>
+                    Preview:
+                    <img src={URL.createObjectURL(value)} alt={value.name} className="w-full rounded-md shadow-md" />
+                    <ul>
+                        <li>Name: {value.name}</li>
+                        <li>Type: {value.type}</li>
+                        <li>Size: {value.size} bytes</li>
+                    </ul>
+                </div>
+            )}
         </>
     )
 }
