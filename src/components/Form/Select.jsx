115902ae37b4048406637837dcc0ea4e89295129
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-30 15:46:36 +0100

    applied floating label classes

--- src/components/Form/Select.jsx
+++ src/components/Form/Select.jsx
@@ -1,18 +1,19 @@
 export default function Select({ label, value, setValue, options, className = "", ...props }) {
+    // TODO: darkmode dropdown menu color
     return (
-        <div className="relative">
+        <div className={`relative ${className}`}>
             <label>
                 <select
                     value={value}
                     onChange={(e) => setValue(e.target.value)}
                     {...props}
-                    className={`${className} w-full p-2 rounded-md transition-[box-shadow] duration-200 bg-transparent placeholder-transparent outline-none border border-gray-400 dark:border-gray-500 focus:border-primary focus:ring-1 focus:ring-primary peer`}
+                    className="floating-label-input peer"
                 >
                     {options.map(({ value, label }) => (
                         <option key={value} value={value}>{label}</option>
                     ))}
                 </select>
-                <span className="absolute left-1.5 -top-2.5 peer-focus:-top-2.5 peer-placeholder-shown:top-2.5 text-sm peer-focus:text-sm peer-placeholder-shown:text-base px-0.5 text-inherit peer-placeholder-shown:text-gray-400 bg-floating-light dark:bg-floating-dark transition-[color,top,font-size] duration-200 peer-focus:text-primary peer-required:after:[*]">
+                <span className="floating-label">
                     {label}
                 </span>
             </label>
