40f877c6104e13f4725e3fe0827862f9f19f8462
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-30 23:02:54 +0100

    changed value to checked prop

--- src/components/Form/Checkbox.jsx
+++ src/components/Form/Checkbox.jsx
@@ -1,9 +1,9 @@
-export default function Checkbox({ label, value, setValue, className = "", required = false, ...props }) {
+export default function Checkbox({ label, checked, setChecked, className = "", required = false, ...props }) {
     // TODO: fix hover + background color
     return (
         <label className="flex items-center gap-2">
-            <input id="default-checkbox" type="checkbox" value={value} onChange={(e) => setValue(e.target.checked)} required={required} {...props} className={`${className} w-4 h-4 accent-primary outline-none peer`} />
-            {label}{required && <span className="text-danger"> *</span>}
+            <input id="default-checkbox" type="checkbox" checked={checked} onChange={() => setChecked(!checked)} required={required} {...props} className={`${className} w-4 h-4 accent-primary outline-none peer`} />
+            <span>{label}{required && <span className="text-danger"> *</span>}</span>
         </label>
     )
 }
