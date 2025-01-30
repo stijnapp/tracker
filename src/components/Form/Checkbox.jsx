597ca207c18a6568f9b4c9fbdf524f63a9e86c47
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-30 17:36:16 +0100

    small Checkbox fix

--- src/components/Form/Checkbox.jsx
+++ src/components/Form/Checkbox.jsx
@@ -1,8 +1,8 @@
 export default function Checkbox({ label, value, setValue, className = "", required = false, ...props }) {
-    // TODO: fix colors
+    // TODO: fix hover + background color
     return (
         <label className="flex items-center gap-2">
-            <input id="default-checkbox" type="checkbox" value={value} onChange={(e) => setValue(e.target.checked)} required={required} {...props} className={`${className} w-4 h-4 accent-primary peer`} />
+            <input id="default-checkbox" type="checkbox" value={value} onChange={(e) => setValue(e.target.checked)} required={required} {...props} className={`${className} w-4 h-4 accent-primary outline-none peer`} />
             {label}{required && <span className="text-danger"> *</span>}
         </label>
     )
