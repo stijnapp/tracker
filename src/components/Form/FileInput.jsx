ad6481bc069c25eec6389b08d83b2ba77973bb4b
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-01-30 01:00:30 +0100

    more test-features for FileInput

--- src/components/Form/FileInput.jsx
+++ src/components/Form/FileInput.jsx
@@ -1,10 +1,11 @@
-export default function FileInput({ label, setValue, className = "", ...props }) {
+export default function FileInput({ label, value, setValue, className = "", ...props }) {
     const handleFileChange = (e) => {
         if (!e.target.files) return
         setValue(e.target.files[0])
     }
 
     // TODO: add styling
+    // TODO: add remove button
     return (
         <>
             {label}
@@ -14,6 +15,15 @@ export default function FileInput({ label, setValue, className = "", ...props })
                 {...props}
                 className={`${className}`}
             />
+            {value && <>
+                Preview:
+                <img src={URL.createObjectURL(value)} alt={value.name} className="w-full rounded-md shadow-md" />
+                <ul>
+                    <li>Name: {value.name}</li>
+                    <li>Type: {value.type}</li>
+                    <li>Size: {value.size} bytes</li>
+                </ul>
+            </>}
         </>
     )
 }
