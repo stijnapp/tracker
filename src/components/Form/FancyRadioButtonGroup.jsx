e4c2d8ec48243a69907d832c32009510183bc9f5
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-02-01 00:39:07 +0100

    jsdoc update + formatting

--- src/components/Form/FancyRadioButtonGroup.jsx
+++ src/components/Form/FancyRadioButtonGroup.jsx
@@ -14,7 +14,6 @@ import Badge from "../Badge";
  * @param {string} props.label - The label of the radio button group
  * @param {Option[]} props.options - The options for the radio button group
  * @param {string} props.value - The value of the selected radio button
- * @param {Function} props.setValue - The function to set the value of the selected radio button
  * @param {string} [props.defaultValue=""] - The default value of the radio button group (will have a badge)
  * @param {boolean} [props.hideLabel=false] - Whether to hide the label
  * @param {string} [props.className=""] - Optional styling for the radio button group
@@ -32,7 +31,15 @@ export default function FancyRadioButtonGroup({ label, options, value, defaultVa
                         <label htmlFor={optionValue} className="flex justify-between gap-4 items-center p-2 rounded-lg ring-1 ring-transparent has-[:checked]:ring-primary has-[:checked]:text-primary has-[:checked]:bg-primary/10">
                             {icon && <FontAwesomeIcon icon={icon} className="aspect-square" />}
                             <p className="flex-auto">{optionLabel}{optionValue === defaultValue && <Badge className="ml-2">Default</Badge>}</p>
-                            <input type="radio" name={label} id={optionValue} value={optionValue} checked={value === optionValue} {...props} className="box-content h-2 w-2 appearance-none rounded-full border-[5px] border-floating-light dark:border-floating-dark bg-floating-light dark:bg-floating-dark bg-clip-padding outline-none ring-1 ring-black/20 dark:ring-white/10 checked:border-primary checked:ring-primary" />
+                            <input
+                                type="radio"
+                                name={label}
+                                id={optionValue}
+                                value={optionValue}
+                                checked={value === optionValue}
+                                {...props}
+                                className="box-content h-2 w-2 appearance-none rounded-full border-[5px] border-floating-light dark:border-floating-dark bg-floating-light dark:bg-floating-dark bg-clip-padding outline-none ring-1 ring-black/20 dark:ring-white/10 checked:border-primary checked:ring-primary"
+                            />
                         </label>
                     </li>
                 ))}
