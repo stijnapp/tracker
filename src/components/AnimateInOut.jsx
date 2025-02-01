381af987a54c111cd3d551d74b6ee532b78fab7c
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-02-01 18:16:45 +0100

    support for animation direction

--- src/components/AnimateInOut.jsx
+++ src/components/AnimateInOut.jsx
@@ -6,11 +6,12 @@ import { getMsFromDuration } from "../helpers/stringManipulation"
  * @param {Object} props
  * @param {boolean} [props.restartOnChildKeyChange=false] - If true, will close and reopen the component when the key of the child changes
  * @param {"vertical" | "horizontal" | "both"} [props.direction="vertical"] - The direction of the transition
+ * @param {string} [props.hiddenClassName=""] - Optional styling for the hidden state of the component (e.g. '-mt-4')
  * @param {string} [props.className=""] - Optional styling for the component
  * @param {JSX.Element} props.children - The children to animate
  * @returns {JSX.Element | null} The animated component
  */
-export default function AnimateInOut({ restartOnChildKeyChange = false, /* direction = "vertical", */ className = "", children }) {
+export default function AnimateInOut({ restartOnChildKeyChange = false, direction = "vertical", hiddenClassName = '', className = "", children }) {
     // TODO: Implement direction
     const [isHiding, setIsHiding] = useState(true)
     const [childrenCache, setChildrenCache] = useState(children)
@@ -51,8 +52,20 @@ export default function AnimateInOut({ restartOnChildKeyChange = false, /* direc
 
     if (!children && !childrenCache) return null
 
+    const hiddenSizes = {
+        vertical: 'h-0 py-0 my-0',
+        horizontal: 'w-0 px-0 mx-0',
+        both: 'h-0 w-0 p-0 p-0'
+    }
+
+    const shownSizes = {
+        vertical: 'h-auto',
+        horizontal: 'w-[calc-size(auto,size)]',
+        both: 'h-auto w-auto'
+    }
+
     return (
-        <div className={`${className} ${isHiding ? 'h-0 py-0 my-0 opacity-0 ease-in' : 'h-auto opacity-100 ease-out'} transition-[height,padding,margin,opacity] ${duration} overflow-hidden -m-2 p-2`}>
+        <div className={`${className} ${isHiding ? `${hiddenSizes[direction] || hiddenSizes['vertical']} opacity-0 ${hiddenClassName}` : `${shownSizes[direction] || shownSizes['vertical']} opacity-100`} transition-[height,width,padding,margin,opacity] ${duration} overflow-hidden -m-2 p-2`}>
             {!isHiding && children ? children : childrenCache}
         </div>
     )
