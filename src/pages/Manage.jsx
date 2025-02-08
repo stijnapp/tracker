8c5d2a61123c0600029ae3b8a0ada7c969ab9229
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-02-08 17:27:59 +0100

    todo: tabs

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -43,6 +43,18 @@ export default function Manage() {
 
     return (
         <Page title="Manage">
+            {/* TODO: tabs */}
+            {/* <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+                <ul className="flex flex-wrap -mb-px">
+                    <li className="me-2">
+                        <a href="#" className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">Exercises</a>
+                    </li>
+                    <li className="me-2">
+                        <a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Tags</a>
+                    </li>
+                </ul>
+            </div> */}
+
             <Card title={<>Search <FontAwesomeIcon icon={faSearch} className="w-4 h-4" /></>}>
                 <div className="flex gap-2 mt-1">
                     <Input type="text" inputMode="search" value={search} onChange={handleSearchChange} label="Exercise name" className="flex-grow" />
