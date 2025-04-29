aa95b6b3ac3bc209dadceef9cf5c3045b385d455
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-04-29 22:52:29 +0200

    fixed notFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,10 +3,10 @@ import Page from '../components/Page'
 
 export default function NotFound() {
     return (
-        <Page>
+        <Page title="">
             <div className="flex flex-col gap-4 items-center text-center">
                 <h1 className="text-7xl tracking-tight font-extrabold text-primary">404</h1>
-                <p className="text-3xl tracking-tight font-bold text-gray-900">Something&apos;s missing</p>
+                <p className="text-3xl tracking-tight font-bold">Something&apos;s missing</p>
                 <p>Sorry, we can&apos;t find that page. You&apos;ll find lots to explore on the home page</p>
                 <Link to="/" className="btn-primary">Back to home</Link>
             </div>
